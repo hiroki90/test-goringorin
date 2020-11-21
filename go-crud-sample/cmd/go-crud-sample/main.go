099321b3131d1eace0b2cd348c568d7f47380aa8
Suzuki Takeo <takeo@stko.info>
2020-11-21 20:24:37 +0900

    set account handler

--- go-crud-sample/cmd/go-crud-sample/main.go
+++ go-crud-sample/cmd/go-crud-sample/main.go
@@ -1,13 +1,31 @@
 package main
 
 import (
+	"github.com/gin-gonic/gin"
 	"github.com/hiroki90/goringorin/go-crud-sample/db"
+	"github.com/hiroki90/goringorin/go-crud-sample/internal"
 	"log"
+	"os"
 )
 
 func main() {
-	_,err:=db.NewDBConnection()
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+
+	conn, err := db.NewDBConnection()
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	accountsRepository := internal.NewAccountsRepository(conn)
+	accountsHandler := internal.NewAccountsHandler(accountsRepository)
+
+	e := gin.Default()
+
+	e.POST("/accounts", accountsHandler.CreateAccountHandle)
+	e.GET("/accounts/:accountID", accountsHandler.GetAccountHandle)
+
+	_ = e.Run(":" + port)
 }
